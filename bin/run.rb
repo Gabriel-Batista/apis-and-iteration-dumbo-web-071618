a25964431208968f4cf39c1ea76a8bff63779fcb
Gabriel Batista <gabedbatista@gmail.com>
2018-07-17 17:41:23 -0400

    Update run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,12 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
+require 'rest-client'
+character_data = RestClient.get('http://swapi.co/api/people')
+
+
 welcome
-character = get_character_from_user
-show_character_movies(character)
+input = get_input_from_user
+ unless show_character_movies(input)
+    get_movie_info(input)
+ end
